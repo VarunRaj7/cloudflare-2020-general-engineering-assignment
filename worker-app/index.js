7756be66f571f2dde93e789634a4b7fbc1e2fb21
VarunRaj7 <rvarunraj1@gmail.com>
2020-10-19 20:17:07 -0400

    adding the links to show up on the static page

--- worker-app/index.js
+++ worker-app/index.js
@@ -13,6 +13,20 @@ addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
 
+class LinksTransformer {
+    constructor(links) {
+        this.links = links
+    }
+
+    async element(element) {
+        this.links.map(link => {
+            element.append(`<a href=${link.url}>${link.name}</a>`, {
+                html: true,
+            })
+        })
+    }
+}
+
 function linksHandler(request) {
     const init = {
         headers: { 'content-type': 'application/json' },
@@ -21,10 +35,23 @@ function linksHandler(request) {
     return new Response(body, init)
 }
 
+async function nonLinkshandler(request) {
+    const init = {
+        headers: { 'content-type': 'text/html' },
+    }
+    const body = await fetch(
+        'https://static-links-page.signalnerve.workers.dev'
+    )
+    return new HTMLRewriter()
+        .on('div#links', new LinksTransformer(links))
+        .transform(body)
+}
+
 async function handleRequest(request) {
     const r = new Router()
 
     r.get('/links', () => linksHandler(request))
+    r.get('/*', () => nonLinkshandler(request))
 
     const resp = await r.route(request)
     return resp
