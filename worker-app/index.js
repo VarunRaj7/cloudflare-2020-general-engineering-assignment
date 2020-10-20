8607a20b79a71770be50afdc22156cc76612a4d1
VarunRaj7 <rvarunraj1@gmail.com>
2020-10-19 20:23:24 -0400

    adding social links

--- worker-app/index.js
+++ worker-app/index.js
@@ -14,6 +14,24 @@ const links = [
     { name: 'Twitter', url: 'https://twitter.com/' },
 ]
 
+const socialLinks = [
+    {
+        name: 'GitHub',
+        url: 'https://github.com/VarunRaj7',
+        img: 'https://simpleicons.org/icons/github.svg',
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/varun-raj-rayabarapu/',
+        img: 'https://simpleicons.org/icons/linkedin.svg',
+    },
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/VarunRajR4',
+        img: 'https://simpleicons.org/icons/twitter.svg',
+    },
+]
+
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
@@ -68,6 +86,22 @@ class SetAttrTransformer {
     }
 }
 
+class SocialLinksTransformer {
+    constructor(socialLinks) {
+        this.socialLinks = socialLinks
+    }
+    async element(element) {
+        this.socialLinks.map(link =>
+            element.append(
+                `<a href=${link.url}><img style="filter: brightness(0) invert(1);" src=${link.img} alt=${link.name}></a>`,
+                {
+                    html: true,
+                }
+            )
+        )
+    }
+}
+
 async function nonLinkshandler(request) {
     const init = {
         headers: { 'content-type': 'text/html' },
@@ -80,6 +114,8 @@ async function nonLinkshandler(request) {
         .on('div#profile', new RemoveAttrTransformer('style'))
         .on('h1#name', new SetContentTransformer(userName))
         .on('img#avatar', new SetAttrTransformer('src', userImage))
+        .on('div#social', new RemoveAttrTransformer('style'))
+        .on('div#social', new SocialLinksTransformer(socialLinks))
         .transform(body)
 }
 
